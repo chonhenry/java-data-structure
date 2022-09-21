23210533cf0026f7ce3b02fe0d9d277bc9733a81
chonhenry <chonhenry@gmail.com>
2022-09-21 19:39:41 -0400

    arrays

---
+++ src/main/java/com/henry/TheArrays.java
@@ -0,0 +1,37 @@
+package com.henry;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        colors[2] = "yellow";
+        System.out.println(colors[2]);
+
+        int[] numbers = {100, 200};
+
+        System.out.println(colors.length);
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        for(String color : colors) {
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
+    }
+}
+// 9:20
